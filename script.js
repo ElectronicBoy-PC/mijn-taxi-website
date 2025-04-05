9023ab7b7755939fa3156115d794b79276f1b7ea
ElectronicBoy-PC <stephan.fonteyne@gmail.com>
2025-04-05 18:38:55 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// script.js
+document.querySelector(".booking-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("Boeking verzonden! Je ontvangt een bevestiging.");
+});
